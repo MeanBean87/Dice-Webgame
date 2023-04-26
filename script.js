27c19010284e4ab71a269eee617e48b597f3fb32
MeanBean87 <58374225+MeanBean87@users.noreply.github.com>
2023-04-25 23:02:31 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function rollDice() {
+    var image1 = document.getElementById("img1");
+    var image2 = document.getElementById("img2");
+
+    var randomNumber1 = Math.floor(Math.random * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random * 6) + 1;
+    
+    image1.src = "./images/dice-" + randomNumber1 + ".svg";
+    image2.src = "./images/dice-" + randomNumber2 + "-fill.svg";
+
+    image1.alt = "Player one's dice is " + randomNumber1;
+    image2.alt = "Player two's dice is " + randomNumber2;
+
+    if (randomNumber1 > randomNumber2){
+        document.getElementById("h2").textContent = "Player 1 is the Winner!";
+    }else if (randomNumber1 < randomNumber2){
+        document.getElementById("h2").textContent = "Player 2 is the Winner!";
+    }else{
+        document.getElementById("h2").textContent = "Its a Draw. Try again."
+    }
+}
+
+rollDice();
